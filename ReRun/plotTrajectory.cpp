60adec1a291832776cd4d58e0b4d104c8b1bc9ea
Dms-zi <em309@syuin.ac.kr>
2025-07-23 15:10:38 +0900

    plotTrajectory with ReRun main

--- ReRun/plotTrajectory.cpp
+++ ReRun/plotTrajectory.cpp
@@ -24,24 +24,22 @@ translation3D -> translation
 LineStrips3D -> set of Linestrip3D
 tick -> timecolumn, rerun viewer timeline
 */
-int main(int argc, char **argv){
-
-    const auto rec = rerun::RecordingStream("plotTrajectory with Rerun");
-    rec.spawn().exit_on_failure();
-
-
+int main(int argc, char **argv)
+{
     vector<RotationQuat> q;
     vector<Translation3D> t;
     vector<int64_t> ts;
 
     ifstream fin(trajectory_file);
 
-    if(!fin){
+    if(!fin)
+    {
         cout << "cannot find trajectory file at" << trajectory_file << endl;
         return 1;
     }
 
-    while (!fin.eof()){
+    while (!fin.eof())
+    {
         double tx, ty, tz, qx, qy, qz, qw;
         string time;
         fin >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
@@ -60,12 +58,14 @@ int main(int argc, char **argv){
             static_cast<float>(ty),
             static_cast<float>(tz)));  
     }
+
     cout << "Loaded " << ts.size() << " points" <<endl;
 
-    // for (size_t i = 0; i < q.size(); ++i) {
-    // auto quat = static_cast<Quaternion>(q[i]);
+    // for (size_t i = 0; i < q.size(); ++i) 
+    // {
+    //      auto quat = static_cast<Quaternion>(q[i]);
 
-    // cout << "Index " << i << ": "
+    //      cout << "Index " << i << ": "
     //           << "w = " << quat.w()
     //           << ", x = " << quat.x()
     //           << ", y = " << quat.y()
@@ -73,35 +73,41 @@ int main(int argc, char **argv){
     //           << endl;
     // }
 
-    // for (size_t i = 0; i < ts.size(); ++i) {
-    // std::cout << "timestamp[" << i << "] = " << ts[i] << " ns" << std::endl;
+    // for (size_t i = 0; i < ts.size(); ++i) 
+    // {
+    // cout << "timestamp[" << i << "] = " << ts[i] << " ns" << endl;
     // }
+    const auto rec = rerun::RecordingStream("plotTrajectory with Rerun");
+    rec.spawn().exit_on_failure();
 
     rec.set_time_sequence("timestamp", ts[0]);
 
-    rec.log(
+    rec.log
+    (
         "poses",
         Points3D(t[0]).with_colors(rerun::Color(0, 255, 0)),
         // Boxes3D::from_half_sizes({{.5f, .5f, .5f}}).with_fill_mode(rerun::FillMode::Solid),
         Transform3D().with_axis_length(5.0)
     );
 
-    rec.send_columns(
+    rec.send_columns
+    (
         "poses",
         TimeColumn::from_sequence("timestamp", ts),
         Transform3D()
         .with_many_translation(t)
         .with_many_quaternion(q)
         .columns()
-        );
+    );
 
 
     LineStrip3D trajectory(t);
 
-    rec.log(
-    "trajectory",
-    LineStrips3D(trajectory)
-    .with_colors({rerun::Color(255, 0, 0)})
+    rec.log
+    (
+        "trajectory",
+        LineStrips3D(trajectory)
+        .with_colors({rerun::Color(255, 0, 0)})
     );
 
     return 0;
